823d53b9e34c3bc614fa1f54dcc45c07877c7759
Adam Sweeney <adam.a.sweeney@gmail.com>
2017-08-13 15:38:59 -0400

    Use Production roots

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,11 +12,10 @@ export const SIGNUP_USER = 'SIGNUP_USER';
 export const CONTACTS_ERROR = 'CONTACTS_ERROR';
 
 const ROOT_URL = 'https://inkblot-contacts.herokuapp.com/api';
-const LOCAL_URL = 'http://localhost:3000/api';
 
 export function signinUser(values, callback) {
 	return function(dispatch) {
-		axios.post(`${LOCAL_URL}/login`, values)
+		axios.post(`${ROOT_URL}/login`, values)
 			.then(response => {
 				dispatch({ type: AUTH_USER });
 				localStorage.setItem('token', response.data.access_token);
@@ -30,7 +29,7 @@ export function signinUser(values, callback) {
 
 export function signupUser(values, callback) {
 	return function(dispatch) {
-		axios.post(`${LOCAL_URL}/signup`, values)
+		axios.post(`${ROOT_URL}/signup`, values)
 			.then(response => {
 				dispatch({ type: AUTH_USER });
 				localStorage.setItem('token', response.data.access_token);
@@ -56,7 +55,7 @@ export function signoutUser() {
 
 export function createContact(values, callback) {
 	return function(dispatch) {
-		axios.post(`${LOCAL_URL}/contacts`, values, {
+		axios.post(`${ROOT_URL}/contacts`, values, {
 			headers: { authorization: localStorage.getItem('token') }
 		}).then(response => {
 				dispatch({
@@ -70,7 +69,7 @@ export function createContact(values, callback) {
 
 export function fetchContacts() {
 	return function(dispatch) {
-	  axios.get(`${LOCAL_URL}/contacts`, {
+	  axios.get(`${ROOT_URL}/contacts`, {
 			headers: { authorization: localStorage.getItem('token')}
 		}).then(response => {
 			dispatch({
@@ -85,7 +84,7 @@ export function fetchContacts() {
 
 export function fetchContact(id) {
 	return function(dispatch) {
-	  axios.get(`${LOCAL_URL}/contacts/${id}`, {
+	  axios.get(`${ROOT_URL}/contacts/${id}`, {
 			headers: { authorization: localStorage.getItem('token')}
 		}).then(response => {
 			dispatch({
@@ -100,7 +99,7 @@ export function fetchContact(id) {
 
 export function updateContact(values, id, callback) {
 	return function(dispatch) {
-		axios.put(`${LOCAL_URL}/contacts/${id}`, values, {
+		axios.put(`${ROOT_URL}/contacts/${id}`, values, {
 			headers: { authorization: localStorage.getItem('token') }
 		}).then(response => {
 				dispatch({
@@ -114,7 +113,7 @@ export function updateContact(values, id, callback) {
 
 export function deleteContact(id, callback) {
 	return function(dispatch) {
-	  axios.delete(`${LOCAL_URL}/contacts/${id}`, {
+	  axios.delete(`${ROOT_URL}/contacts/${id}`, {
 			headers: { authorization: localStorage.getItem('token') }
 		}).then(() => {
 			dispatch({
